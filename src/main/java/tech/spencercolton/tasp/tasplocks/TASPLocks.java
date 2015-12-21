18b979fa56d15112b1ce5b1c2ddda60e5a277fa5
Spencer Colton <me@spencercolton.tech>
2015-12-20 22:34:34 -0500

    Update Repo name  and dependencies

--- src/main/java/tech/spencercolton/tasp/tasplocks/TASPLocks.java
+++ src/main/java/tech/spencercolton/tasp/tasplocks/TASPLocks.java
@@ -2,6 +2,7 @@ package tech.spencercolton.tasp.tasplocks;
 
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
+import tech.spencercolton.tasp.Util.Config;
 
 public class TASPLocks extends JavaPlugin {
 
@@ -9,8 +10,8 @@ public class TASPLocks extends JavaPlugin {
         Plugin TASP = getServer().getPluginManager().getPlugin("TASP");
 
         if(TASP == null) {
-            getLogger().severe(tech.spencercolton.tasp.Util.Config.err() + "Unable to load TASP Doors plugin because the TASP main plugin is missing!");
-            getLogger().severe(tech.spencercolton.tasp.Util.Config.err() + "Install the TASP main plugin before trying again.");
+            getLogger().severe(Config.err() + "Unable to load TASP Doors plugin because the TASP main plugin is missing!");
+            getLogger().severe(Config.err() + "Install the TASP main plugin before trying again.");
             getServer().getPluginManager().disablePlugin(this);
         }
     }
