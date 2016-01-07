f3411f4630b8aaf625a2215b5b7f88b23855bd76
Spencer Colton <me@spencercolton.tech>
2016-01-06 22:31:39 -0500

    Init

--- src/main/java/tech/spencercolton/tasp/tasplocks/TASPLocks.java
+++ src/main/java/tech/spencercolton/tasp/tasplocks/TASPLocks.java
@@ -6,18 +6,20 @@ import tech.spencercolton.tasp.Util.Config;
 
 public class TASPLocks extends JavaPlugin {
 
+    @Override
     public void onEnable() {
-        Plugin TASP = getServer().getPluginManager().getPlugin("TASP");
+        Plugin TASP = this.getServer().getPluginManager().getPlugin("TASP");
 
         if(TASP == null) {
-            getLogger().severe(Config.err() + "Unable to load TASP Doors plugin because the TASP main plugin is missing!");
-            getLogger().severe(Config.err() + "Install the TASP main plugin before trying again.");
-            getServer().getPluginManager().disablePlugin(this);
+            this.getLogger().severe(Config.err() + "Unable to load TASP Doors plugin because the TASP main plugin is missing!");
+            this.getLogger().severe(Config.err() + "Install the TASP main plugin before trying again.");
+            this.getServer().getPluginManager().disablePlugin(this);
         }
     }
 
+    @Override
     public void onDisable() {
-        getLogger().info("Disabling TASP Doors plugin...");
+        this.getLogger().info("Disabling TASP Doors plugin...");
     }
 
 }
